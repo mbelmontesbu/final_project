79b43b9820fb86dda927ec966e5fc49bb6942043
mbelmontesbu <michael.belmonte@stonybrook.edu>
2022-03-30 14:20:44 -0400

    Created a color class. The class takes a char and allows access to that color's output string. For use with constructing the letter block.

--- wordle_project.cpp
+++ wordle_project.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "letter_block.H"
 
+#include "color.H"
+
 int main() {
     std::cout << "\u001b[30m \u001b[47m A \u001b[0m : Letter is not in the word." << std::endl;
     std::cout << "\u001b[30m \u001b[43m A \u001b[0m : Letter is in the word, but not in the correct place." << std::endl;
@@ -8,4 +10,11 @@ int main() {
 
     letter_block letter1;
     std::cout << letter1 << std::endl;
+
+    color green('g');
+    std::cout << green.get_char() << std::endl;
+
+    letter_block letter2('A', green, 1);
+    std::cout << letter2 << std::endl;
+
 }
