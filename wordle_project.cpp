7f3f552015248c6844b4b83a5e0613290787279f
mbelmontesbu <michael.belmonte@stonybrook.edu>
2022-05-03 14:53:04 -0400

    added ways to count how many letters in the word added funciton to print a vector of letter blocks moved dolor definitions to outside of main

--- wordle_project.cpp
+++ wordle_project.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <algorithm>
 
 #include "color.H"
 #include "letter_block.H"
 
+struct occ_of_letter
+{
+    char c;
+    int count;
+};
+
+//initialize constants
+//colors
+color green('g');
+color yellow('y');
+color white('w');
+//other
+std::vector<std::vector<letter_block>> previous_guesses;
+
+//initialize functions
+std::vector<occ_of_letter> count_letters(std::string &word);
+void print_word(std::vector<letter_block> &word);
+
 int main() {
     std::cout << "\u001b[30m \u001b[47m A \u001b[0m : Letter is not in the word." << std::endl;
     std::cout << "\u001b[30m \u001b[43m A \u001b[0m : Letter is in the word, but not in the correct place." << std::endl;
     std::cout << "\u001b[30m \u001b[42m A \u001b[0m : Letter is in  the word and in the correct place." << std::endl;
-    //initialize colors
-    color green('g');
-    color yellow('y');
-    color white('w');
+    
 
     /*
     char character_in;
@@ -57,6 +73,15 @@ int main() {
         std::cout<< "AHH" << std::endl;
     }*/
 
+    //count instances of letters
+    std::vector<occ_of_letter> entry_counts = count_letters(entry);
+
+    for (auto e: entry_counts){
+    std::cout << e.c << std::endl;
+    std::cout << e.count << std::endl;
+    }
+    
+    std::vector<letter_block> blocked_word;
     for (auto it = entry.begin(); it < entry.end(); ++it) {
         letter_block letter(*it,white,it-entry.begin());
         auto pos = std::find(answer.begin(), answer.end(),*it);
@@ -73,7 +98,51 @@ int main() {
             letter.change_color(yellow);
             std::cout << letter;
         }
-        
+        blocked_word.push_back(letter);
+    }
+    previous_guesses.push_back(blocked_word);
+    
+    for(int i = 0; i < 5; ++i){
+        letter_block letter(entry[i],white,i);
+        auto pos = std::find(answer.begin(), answer.end(), entry[i]);
+        if(pos == answer.end()){ //letter not in word
+            std::cout << letter;
+            //continue;
+        }
+        else if(pos-answer.begin() == i){ //letter in word and correct position
+            letter.change_color(green);
+            std::cout << letter;
+            //continue;
+        }
+        else{
+            letter.change_color(yellow); //letter in word but no correct position
+            std::cout << letter;
+        }
+        blocked_word.push_back(letter);
+    }
+    previous_guesses.push_back(blocked_word);
+    std::cout<<std::endl;
+
+    for (auto e: previous_guesses){
+        print_word(e);
+    }
+}
+
+std::vector<occ_of_letter> count_letters(std::string &word){
+    std::vector<occ_of_letter> occ_of_letters;
+    for (auto it = word.begin(); it < word.end(); ++it) {
+        occ_of_letter state{};
+        int count = std::count(word.begin(), word.end(), *it);
+        state.c = *it;
+        state.count = count;
+        occ_of_letters.push_back(state);   
+    }
+    return occ_of_letters;
+};
+
+void print_word(std::vector<letter_block> &word){
+    for (auto e:word){
+        std::cout<<e;
     }
     std::cout<<std::endl;
-}
+};
