36708f07ff065d27f39debfae5658ab6e95ca60c
mbelmontesbu <michael.belmonte@stonybrook.edu>
2022-05-05 23:46:50 -0400

    improved the way the words compare to evaluate double letters easier

--- wordle_project.cpp
+++ wordle_project.cpp
@@ -19,6 +19,7 @@ color yellow('y');
 color white('w');
 //other
 std::vector<std::vector<letter_block>> previous_guesses;
+int number_of_guesses;
 
 //initialize functions
 std::vector<occ_of_letter> count_letters(std::string &word);
@@ -29,104 +30,74 @@ int main() {
     std::cout << "\u001b[30m \u001b[43m A \u001b[0m : Letter is in the word, but not in the correct place." << std::endl;
     std::cout << "\u001b[30m \u001b[42m A \u001b[0m : Letter is in  the word and in the correct place." << std::endl;
     
+    int state_of_game = 0;
 
-    /*
-    char character_in;
-    std::cout << "Enter Letter: ";
-    std::cin >> character_in;
+    while (state_of_game == 0){
+        number_of_guesses += 1;
+        std::string answer{"sheep"};
 
-    letter_block letter3(character_in, yellow, 1);
-    std::cout << letter3 << std::endl;
-    letter3.change_color(green);
-    std::cout << letter3 << std::endl;
-
-    
-    std::string string_in{"default"};
-    while(string_in.size()<1 || string_in.size()>5){
-        std::cout << "Enter a 5-letter word: ";
-        std::cin >> string_in;
-    }
-
-    //std::string answer{"sheep"};
-    for (auto& s : string_in){
-        std::cout << letter_block(s,white,1);
-    }
-    std::cout<<std::endl;
-    */
-
-    std::string answer{"shemp"};
-    /*
-    auto pos1 = std::find(answer.cbegin(), answer.cend(), 'e');
-    std::cout << "element found: " << *pos1 << std::endl;
-    */
-    std::string entry{"default"};
-    while(entry.size()<1 || entry.size()>5){
-        std::cout << "Enter a 5-letter word: ";
-        std::cin >> entry;
-    }
-
-    /*
-    auto pos2 = std::find(entry.cbegin(), entry.cend(), 'e');
-    std::cout << "element found: " << *pos2 << std::endl;
-
-    if(pos1 - answer.cbegin() == pos2 - entry.cbegin()){
-        std::cout<< "AHH" << std::endl;
-    }*/
-
-    //count instances of letters
-    std::vector<occ_of_letter> entry_counts = count_letters(entry);
-
-    for (auto e: entry_counts){
-    std::cout << e.c << std::endl;
-    std::cout << e.count << std::endl;
-    }
-    
-    std::vector<letter_block> blocked_word;
-    for (auto it = entry.begin(); it < entry.end(); ++it) {
-        letter_block letter(*it,white,it-entry.begin());
-        auto pos = std::find(answer.begin(), answer.end(),*it);
-        if(pos == answer.end()){
-            std::cout << letter;
-            //continue;
+        std::string raw_entry{"default"};
+        while(raw_entry.size()<1 || raw_entry.size()>5){
+            std::cout << "Enter a 5-letter word: ";
+            std::cin >> raw_entry;
         }
-        else if(pos-answer.begin() == it-entry.begin()){
-            letter.change_color(green);
-            std::cout << letter;
-            //continue;
-        }
-        else{
-            letter.change_color(yellow);
-            std::cout << letter;
-        }
-        blocked_word.push_back(letter);
-    }
-    previous_guesses.push_back(blocked_word);
-    
-    for(int i = 0; i < 5; ++i){
-        letter_block letter(entry[i],white,i);
-        auto pos = std::find(answer.begin(), answer.end(), entry[i]);
-        if(pos == answer.end()){ //letter not in word
-            std::cout << letter;
-            //continue;
-        }
-        else if(pos-answer.begin() == i){ //letter in word and correct position
-            letter.change_color(green);
-            std::cout << letter;
-            //continue;
-        }
-        else{
-            letter.change_color(yellow); //letter in word but no correct position
-            std::cout << letter;
-        }
-        blocked_word.push_back(letter);
-    }
-    previous_guesses.push_back(blocked_word);
-    std::cout<<std::endl;
 
-    for (auto e: previous_guesses){
-        print_word(e);
+        std::string entry;
+        for (auto e:raw_entry){
+            char c = tolower(e);
+            entry.push_back(c);
+        }
+
+        std::vector<letter_block> blocked_word;
+
+
+    //testing compare
+        std::vector<letter_block> answer_word;
+        for(int i = 0; i<5; ++i){
+            letter_block letter(answer[i],white,i);
+            answer_word.push_back(letter);
+        }
+        
+        std::string comp_answer = answer;
+
+        for(int i = 0; i < 5; ++i){
+            letter_block letter(entry[i],white,i);
+            auto pos = std::find(comp_answer.begin(), comp_answer.end(), entry[i]);
+            if(pos-comp_answer.begin() == i){ //letter in word and correct position
+                letter.change_color(green);
+                comp_answer.replace(pos,pos+1,"A");
+            }
+            blocked_word.push_back(letter);
+        }
+        
+        for(int i = 0; i <5; ++i){
+            auto pos = std::find(comp_answer.begin(), comp_answer.end(), entry[i]);
+            std::cout<< comp_answer <<std::endl;
+            std::cout<< entry[i] << "," << pos-comp_answer.begin() << std::endl;
+            if((pos-comp_answer.begin() != i) && (pos != comp_answer.end()) && (blocked_word[i].get_color().get_char() != green.get_char())){
+                std::cout<< blocked_word[i] <<std::endl;
+                comp_answer.replace(pos,pos+1,"A");
+                blocked_word[i].change_color(yellow);
+            }
+        }
+
+        previous_guesses.push_back(blocked_word);
+
+        for (auto e: previous_guesses){
+            print_word(e);
+        }
+
+        if(entry == answer){
+            state_of_game = 1;
+        }
+        if(number_of_guesses == 6){
+            state_of_game = 1;
+        }
+        
     }
 }
+
+//Define Functions
 
 std::vector<occ_of_letter> count_letters(std::string &word){
     std::vector<occ_of_letter> occ_of_letters;
